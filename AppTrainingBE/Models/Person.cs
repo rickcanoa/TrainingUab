64d0d13530c52622b3a79990123b0291a195270e
Richard Canoa <rcanoa@ucb.cba.edu.bo>
2025-04-03 12:30:47 -0400

    Cambios iniciales de correccion

---
+++ AppTrainingBE/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AppTrainingBE.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string LastName { get; set; }
+        
+        public int Age { get; set; }
+        
+        public DateTime Birthday { get; set; }
+    }
+}
