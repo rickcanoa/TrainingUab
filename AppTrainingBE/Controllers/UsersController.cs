2e823353ddd396e4aabdbc0b7830938f497c2190
Richard Canoa <richard.canoa@uab.edu.bo>
2025-06-05 18:21:05 -0400

    Update de la clase

--- AppTrainingBE/Controllers/UsersController.cs
+++ AppTrainingBE/Controllers/UsersController.cs
@@ -29,12 +29,44 @@ namespace AppTrainingBE.Controllers
             return Ok(users);
         }
 
+        [HttpGet]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _context.Users
+                .Include(u => u.Profile)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if(user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
         [HttpPost]
         public async Task<IActionResult> CreateUser([FromBody] User user)
         { 
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
-            return Ok();
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
         }
-    }
+
+        public async Task<IActionResult> UpdateUser
+            (int id, [FromBody] User updateUser)
+        {
+            var existingUser = await _context.Users
+                .Include(u => u.Profile)
+                .FirstOrDefaultAsync(u => u.Id == id);
+
+            if (existingUser == null)
+                return NotFound();
+
+            //Actualizar los datos
+            existingUser.UserName = updateUser.UserName;
+            existingUser.Profile.FullName = updateUser.Profile.FullName;
+            existingUser.Profile.DateOfBirth = updateUser.Profile.DateOfBirth;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
 }
